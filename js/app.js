f42c9eeac7c9e2c61c898d4c06c481b33dfad955
sargoo <goon_aguera@hotmail.com>
2024-08-09 14:49:21 -0300

    se agregan funciones a la clases ui

--- js/app.js
+++ js/app.js
@@ -7,17 +7,87 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
-    formulario.addEventListener('submit', agregarGasto);
+
+    formulario.addEventListener('submit',agregarGasto);
 }
 
 // clases   
 
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI{
+    insertarPresupuesto(monto){
+
+        //extraer valores
+        const {presupuesto,restante} = monto;
+
+        //agregar al html
+        document.querySelector('#total'). textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje, tipo){
+        //crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center','alert');
+
+        if (tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        }else{
+            divMensaje.classList.add('alert-success');
+        }
+
+        //mensaje
+        divMensaje.textContent = mensaje;
+        //insertar en el html
+
+        document.querySelector('.primario').insertBefore(divMensaje,formulario);
+
+        //borrar el mensaje
+        setTimeout(()=>{
+            divMensaje.remove();
+        }, 3000)
+    }
+}
+500
+//instanciar
+const ui = new UI();
+
+let presupuesto;
 // funciones
 
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('Cual es tu presupuesto?');
-    console.log(presupuestoUsuario);
+    //console.log(presupuestoUsuario);
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload();
+    }
+    
+    //presupuesto valido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
+    ui.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e){
+    e.preventDefault();
+
+    //leer los datos del formulario
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = document.querySelector('#cantidad').value;
+
+    //validar
+    if (nombre === '' || cantidad === '') {
+        ui.imprimirAlerta('ambos campos son obligatorios', 'error');   
+        return;
+    } else if (cantidad <= 0 || isNaN(cantidad)) {
+        ui.imprimirAlerta('cantidad no valida','error');
+        return;
     }
 }
